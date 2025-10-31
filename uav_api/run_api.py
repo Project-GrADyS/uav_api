01f850419dd5a127c4bddcb857c69fa53327c194
Francisco Fleury <franmeifleury@gmail.com>
2025-10-31 17:20:07 -0300

    creating setup for location file and fixing args

--- uav_api/run_api.py
+++ uav_api/run_api.py
@@ -1,9 +1,10 @@
 import subprocess
 
 from uav_api.args import parse_args, write_args_to_env
-
+from uav_api.setup import setup
 def run_with_args(raw_args=None):
     args = parse_args(raw_args)
+    args = setup(args)
     write_args_to_env(args)
     
     process = subprocess.Popen([
@@ -19,6 +20,9 @@ def run_with_args(raw_args=None):
 
     return process
 
-if __name__ == "__main__":
+def main():
     api_process = run_with_args()
     api_process.wait()
+
+if __name__ == "__main__":
+    main()
