4627643c540bf5167b5eadac366d895271f8b850
Francisco Meirelles Fleury <franmeifleury@gmail.com>
2025-12-30 21:59:49 +0100

    adding device and id to response so that gradys-gs can identify response author

--- uav_api/routers/movement.py
+++ uav_api/routers/movement.py
@@ -1,7 +1,8 @@
+from argparse import Namespace
 from time import sleep
 from fastapi import APIRouter, Depends, HTTPException
 from uav_api.copter import Copter
-from uav_api.copter_connection import get_copter_instance
+from uav_api.router_dependencies import get_copter_instance, get_args
 from uav_api.classes.pos import GPS_pos, Local_pos
 
 movement_router = APIRouter(
@@ -10,51 +11,51 @@ movement_router = APIRouter(
 )
 
 @movement_router.post("/go_to_gps/", tags=["movement"], summary="Moves the copter to specified GPS position")
-def go_to_gps(pos: GPS_pos, uav: Copter = Depends(get_copter_instance)):
+def go_to_gps(pos: GPS_pos, uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.go_to_gps(pos.lat, pos.long, pos.alt)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"GO_TO FAIL: {e}")
-    return {"result": f"Going to coord ({pos.lat}, {pos.long}, {pos.alt})"}
+    return {"device": "uav", "id": str(args.sysid), "result": f"Going to coord ({pos.lat}, {pos.long}, {pos.alt})"}
 
 @movement_router.post("/go_to_gps_wait", tags=["movement"], summary="Moves and waits for the copter to get to specified GPS position")
-def go_to_gps_wait(pos: GPS_pos, uav: Copter = Depends(get_copter_instance)):
+def go_to_gps_wait(pos: GPS_pos, uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.go_to_gps(pos.lat, pos.long, pos.alt)
         target_loc = uav.mav_location(pos.lat, pos.long, pos.alt)
         uav.wait_location(target_loc, timeout=60)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"GO_TO FAIL: {e}")
-    return {"result": f"Arrived at coord ({pos.lat}, {pos.long}, {pos.alt})"}
+    return {"device": "uav", "id": str(args.sysid), "result": f"Arrived at coord ({pos.lat}, {pos.long}, {pos.alt})"}
 
 @movement_router.post("/go_to_ned", tags=["movement"], summary="Moves to specified NED position")
-def go_to_ned(pos: Local_pos, uav: Copter = Depends(get_copter_instance)):
+def go_to_ned(pos: Local_pos, uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.go_to_ned(pos.x, pos.y, pos.z) 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"GO_TO FAIL: {e}")
-    return {"result": f"Going to NED coord ({pos.x}, {pos.y}, {pos.z})"}
+    return {"device": "uav", "id": str(args.sysid), "result": f"Going to NED coord ({pos.x}, {pos.y}, {pos.z})"}
 
 @movement_router.post("/go_to_ned_wait", tags=["movement"], summary="Moves and waits for the copter to get to specified NED position")
-def go_to_ned_wait(pos: Local_pos, uav: Copter = Depends(get_copter_instance)):
+def go_to_ned_wait(pos: Local_pos, uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.go_to_ned(pos.x, pos.y, pos.z)
         uav.wait_ned_position(pos)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"GO_TO FAIL: {e}")
-    return {"result": f"Arrived at NED coord ({pos.x}, {pos.y}, {pos.z})"}
+    return {"device": "uav", "id": str(args.sysid), "result": f"Arrived at NED coord ({pos.x}, {pos.y}, {pos.z})"}
 
 @movement_router.post("/drive", tags=["movement"], summary="Drives copter the specified amount in meters")
-def drive(pos: Local_pos, uav: Copter = Depends(get_copter_instance)):
+def drive(pos: Local_pos, uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.drive_ned(pos.x, pos.y, pos.z)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"DRIVE FAIL: {e}")
-    return {"result": "Copter is driving"}
+    return {"device": "uav", "id": str(args.sysid), "result": "Copter is driving"}
 
 @movement_router.post("/drive_wait", tags=["movement"], summary="Drives and waits copter the specified amount in meters")
-def drive_wait(pos: Local_pos, uav: Copter = Depends(get_copter_instance)):
+def drive_wait(pos: Local_pos, uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         current_pos = uav.get_ned_position()
         uav.drive_ned(pos.x, pos.y, pos.z)
@@ -62,21 +63,20 @@ def drive_wait(pos: Local_pos, uav: Copter = Depends(get_copter_instance)):
         uav.wait_ned_position(target_pos)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"DRIVE FAIL: {e}")
-    return {"result": f"Copter arrived at ({target_pos.x}, {target_pos.y}, {target_pos.z})"}
+    return {"device": "uav", "id": str(args.sysid), "result": f"Copter arrived at ({target_pos.x}, {target_pos.y}, {target_pos.z})"}
 
 @movement_router.get("/stop", tags=["movement"])
-def stop(uav: Copter = Depends(get_copter_instance)):
+def stop(uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.stop()
         #uav.ensure_holding()
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"STOP FAIL: {e}")
-    return {"result": "Copter has stopped"}
-
+    return {"device": "uav", "id": str(args.sysid), "result": "Copter has stopped"}
 @movement_router.get("/resume", tags=["movement"])
-def resume(uav: Copter = Depends(get_copter_instance)):
+def resume(uav: Copter = Depends(get_copter_instance), args: Namespace = Depends(get_args)):
     try:
         uav.resume()
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"RESUME FAIL: {e}")
-    return {"result": "Copter has resumed movement"}
+    return {"device": "uav", "id": str(args.sysid), "result": "Copter has resumed movement"}
