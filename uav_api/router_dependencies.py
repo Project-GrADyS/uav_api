448004e722628af83d8708787554c86de16075da
Francisco Meirelles Fleury <franmeifleury@gmail.com>
2026-02-02 03:26:19 +0100

    catching keyboard interrupt in parent process to prevent exceptions messages on interrupt

--- uav_api/router_dependencies.py
+++ uav_api/router_dependencies.py
@@ -3,7 +3,7 @@ from uav_api.args import read_args_from_env
 from uav_api.copter import Copter
 
 copter = None
-scripts_path = None
+args = None
 
 def get_copter_instance(sysid=None, connection=None):
     global copter
@@ -12,9 +12,8 @@ def get_copter_instance(sysid=None, connection=None):
         copter.connect(connection_string=connection)
     return copter
 
-def get_scripts_path():
-    global scripts_path
-    if scripts_path is None:
+def get_args():
+    global args
+    if args is None:
         args = read_args_from_env()
-        scripts_path = Path(args.scripts_path).expanduser()
-    return scripts_path
+    return args
